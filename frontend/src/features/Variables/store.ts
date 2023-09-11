9e41be7cc20cbc0d99a3d7e4ce18a1b08fe49be7
kornalius <dehaen@bewave.io>
2023-09-11 09:49:52 -0400

    Global and local variables

--- frontend/src/features/Variables/store.ts
+++ frontend/src/features/Variables/store.ts
@@ -1,26 +1,50 @@
 import { computed, ref } from 'vue'
 import { defineStore } from 'pinia'
+import { useApp } from '@/features/App/store'
 
 export const useVariables = defineStore('variables', () => {
   const states = ref({})
 
+  const app = useApp()
+
+  const storeName = (name: string): string => {
+    if (name.startsWith('@')) {
+      return name.substring(1)
+    }
+    return `${app.menuId}-${name}`
+  }
+
   const variableNames = computed(() => Object.keys(states.value))
 
-  const variableExists = (name: string): boolean => Object.keys(states.value).includes(name)
+  const globalNames = computed(() => (
+    Object.keys(states.value)
+      .filter((n) => !n.startsWith(`${app.menuId}-`))
+      .map((n) => `@${n}`)
+  ))
 
-  const getVariable = (name: string): unknown => states.value[name]
+  const localNames = computed(() => (
+    Object.keys(states.value)
+      .filter((n) => n.startsWith(`${app.menuId}-`))
+      .map((n) => n.replace(new RegExp(`^${app.menuId}-`), ''))
+  ))
+
+  const variableExists = (name: string): boolean => Object.keys(states.value).includes(storeName(name))
+
+  const getVariable = (name: string): unknown => states.value[storeName(name)]
 
   const setVariable = (name: string, value: unknown): void => {
-    states.value[name] = value
+    states.value[storeName(name)] = value
   }
 
   const unsetVariable = (name: string): void => {
-    delete states.value[name]
+    delete states.value[storeName(name)]
   }
 
   return {
     states,
     variableNames,
+    globalNames,
+    localNames,
     variableExists,
     getVariable,
     setVariable,
